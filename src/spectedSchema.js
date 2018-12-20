54e1d1a958918df5da48220b404d35b15b0648d8
Boyko Amarov <boyko.amarov@gmail.com>
2018-12-20 12:41:33 +0200

    Add rules compilation and application.

--- src/spectedSchema.js
+++ src/spectedSchema.js
@@ -7,11 +7,8 @@ import {
   identity,
   map,
   reduce,
+  compose,
 } from 'ramda';
-
-const applyRules = (rules, data) => {
-  return data;
-};
 
 const simpleTypes = [
   'string',
@@ -19,40 +16,104 @@ const simpleTypes = [
   'integer',
 ];
 
-export const clean = (schema: Object, data, required) => {
-  if (simpleTypes.indexOf(schema.type) > -1) {
-    if (!required && typeof data === 'undefined' && schema.default) {
-      return schema.default;
-    }
-    if (schema.rules) {
-      return applyRules(schema.rules, data);
-    }
-    return data;
+export class Jss {
+  constructor(opts = {}) {
+    const {
+      rules = {},
+    } = opts;
+    this.rules = { ...rules };
+    this.clean = this.clean.bind(this);
+    this.compileRules = this.compileRules.bind(this);
+    this.applyRules = this.applyRules.bind(this);
   }
-  if (schema.properties) {
-    // Clean properties
-    const cleaned = {};
-    const requiredProps = schema.required || [];
 
-    Object.keys(schema.properties).forEach(propKey => {
-      const propsSchema = schema.properties[propKey];
-      const nextData = data[propKey];
-      const isRequired = requiredProps.indexOf(propKey) > -1;
-      const cleanedPart = clean(propsSchema, nextData, isRequired);
-      cleaned[propKey] = cleanedPart;
+  addRule = (name, handler) => {
+    if (typeof this.rules[name] !== 'undefined') {
+      throw new Error('A rule with the same name already exists');
+    }
+    this.rules[name] = handler;
+  };
+
+  compileRules = rulesConfig => {
+    if (!Array.isArray(rulesConfig)) {
+      throw new Error('Improperly configured. Rules should be an array of values');
+    }
+    const handlers = rulesConfig.map(config => {
+      if (Array.isArray(config)) {
+        if (config.length !== 2) {
+          throw new Error('Improperly configured. If a rule is specified as an array it should have length 2.');
+        }
+        const ruleHandler = this.rules[config[0]];
+        return curry(ruleHandler, config[0]);
+      }
+      else {
+        const ruleHandler = this.rules[config];
+        return ruleHandler;
+      }
     });
-    return cleaned;
-  }
-  else if (schema.allOf) {
-    // Collect all properties
-    let cleanedParts = {};
-    schema.allOf.forEach(subSchema => {
-      const cleanedPart = clean(subSchema, data);
-      cleanedParts = { ...cleanedParts, ...cleanedPart };
-    });
-    return cleanedParts;
-  }
-  else if (schema.anyOf) {
-    // Collect all properties
-  }
-};
+
+    const combinedHandler = compose(...handlers);
+    return combinedHandler;
+  };
+
+  applyRules = (rulesConfig, data) => {
+    const handler = this.compileRules(rulesConfig);
+    return handler(data);
+  };
+
+
+  clean = (schema: Object, data, required) => {
+    if (simpleTypes.indexOf(schema.type) > -1) {
+      if (!required && typeof data === 'undefined' && schema.default) {
+        return schema.default;
+      }
+      if (required && typeof data === 'undefined') {
+        throw new Error('Invalid data');
+      }
+      if (schema.rules) {
+        return this.applyRules(schema.rules, data);
+      }
+      return data;
+    }
+    if (schema.properties) {
+      // Clean properties
+      const cleaned = {};
+      const requiredProps = schema.required || [];
+
+      Object.keys(schema.properties).forEach(propKey => {
+        const propsSchema = schema.properties[propKey];
+        const nextData = data[propKey];
+        const isRequired = requiredProps.indexOf(propKey) > -1;
+        const cleanedPart = this.clean(propsSchema, nextData, isRequired);
+        cleaned[propKey] = cleanedPart;
+      });
+      return cleaned;
+    }
+    else if (schema.allOf) {
+      // Collect all properties
+      let cleanedParts = {};
+      schema.allOf.forEach(subSchema => {
+        const cleanedPart = this.clean(subSchema, data);
+        cleanedParts = { ...cleanedParts, ...cleanedPart };
+      });
+      return cleanedParts;
+    }
+    else if (schema.anyOf) {
+      console.log('Skipping validation because of anyOf keyword');
+      return data;
+    }
+    else if (schema.oneOf) {
+      console.log('Skipping validation because of oneOf keyword');
+      return data;
+    }
+
+    if (schema.type === 'array') {
+      return data.map(value => this.clean(schema.items, value));
+    }
+
+    throw new Error('Unsupported schema');
+  };
+  // clean = clean;
+  // compileRules = compileRules;
+  // applyRules = applyRules;
+}
--- test/spectedSchema.js
+++ test/spectedSchema.js
@@ -1,26 +1,55 @@
 import { expect } from 'chai';
-import { clean } from '../src/spectedSchema';
+import sinon from 'sinon';
+import { Jss } from '../src/spectedSchema';
 
 const simpleSchema = require('./simpleSchema.json');
+const simpleSchemaWithRules = require('./simleWithRules.json');
+
 const allOfSchema = require('./allOfSchema.json');
+const arraySchema = require('./arraySchema');
+const complexArraySchema = require('./complexArraySchema.json');
 
 describe('spected-schema', () => {
+  describe('Jss', () => {
+    it('should have empty rules if called with no arguments', () => {
+      const jss = new Jss();
+      expect(jss.rules).to.deep.equal({});
+    });
+    it('should load rules with the addRule method', () => {
+      const jss = new Jss();
+      const handler = sinon.spy();
+      jss.addRule('name', handler);
+      expect(jss).to.have.property('rules').to.deep.equal({
+        name: handler,
+      });
+    });
+    it('should load rules by using the rules option', () => {
+      const handler = sinon.spy();
+      const rules = {
+        name: handler,
+      };
+      const jss = new Jss({ rules });
+      expect(jss).to.have.property('rules').to.deep.equal({
+        name: handler,
+      });
+    });
+  });
   describe('simpleSchemas', () => {
     it('should return the data without changes if no rules are specified', () => {
-
       const data = {
         id: 'testId',
         name: 'testName',
       };
-      const result = clean(simpleSchema, data);
-
+      const jss = new Jss();
+      const result = jss.clean(simpleSchema, data);
       expect(result).to.deep.equal(data);
     });
     it('should insert the default property if the value is missing', () => {
       const data = {
         name: 'testName',
       };
-      const result = clean(simpleSchema, data);
+      const jss = new Jss();
+      const result = jss.clean(simpleSchema, data);
       expect(result)
         .to
         .deep
@@ -37,8 +66,60 @@ describe('spected-schema', () => {
         name: 'testName',
         email: 'test@email.com',
       };
-      const result = clean(allOfSchema, data);
+      const jss = new Jss();
+      const result = jss.clean(allOfSchema, data);
       expect(result).to.deep.equal(data);
+    });
+  });
+  describe('type === array', () => {
+    it('should clean simple array elements', () => {
+      const data = [
+        'a',
+        'b',
+      ];
+      const jss = new Jss();
+      const result = jss.clean(arraySchema, data);
+      expect(result).to.deep.equal(data);
+    });
+    it('should complex array elements', () => {
+      const data = {
+        names: [
+          {
+            firstName: 'fn1',
+            lastName: 'ln1',
+          },
+          {
+            firstName: 'fn2',
+            lastName: 'ln2',
+          },
+        ],
+        ids: [
+          'a',
+          'b',
+        ],
+      };
+      const jss = new Jss();
+      const result = jss.clean(complexArraySchema, data);
+      expect(result).to.deep.equal(data);
+    });
+  });
+  it.skip('should throw on unsupported schemas', () => {
+  });
+  it.skip('should throw on invalid data', () => {
+  });
+  describe('should apply single config (TODO fix) rules', () => {
+    it('should apply rules on a leaf node', () => {
+      const data = {
+        id: 'testId',
+        name: 'testName',
+      };
+      const jss = new Jss();
+      const handler = sinon.stub().callsFake(x => x);
+      jss.addRule('trim', handler);
+
+      jss.clean(simpleSchemaWithRules, data);
+      sinon.assert.calledOnce(handler);
+      sinon.assert.calledWith(handler, 'testName');
     });
   });
 });
